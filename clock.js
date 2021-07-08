7b4a04f519a4752da072f0e3cd1c5268d39ecb41
Kobe <kobenorcross@gmail.com>
2021-07-08 13:03:12 -0700

    testing website. i think it is finished

--- clock.js
+++ clock.js
@@ -1,5 +1,4 @@
 setInterval(setClock, 1000)
-
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
@@ -15,8 +14,9 @@ function setClock() {
     setRotation(hourHand, hoursRatio)
 }
 
+
 function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360)
+    element.stlye.setProperty('--rotation', rotationRatio * 360)
 }
 
 setClock()
